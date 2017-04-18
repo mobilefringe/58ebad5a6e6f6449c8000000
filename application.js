a29c0b3d499288028748bd2b3c011782d9960a0b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-18 12:46:19 -0400

    Fleur de Lys 1.0  /application.js

--- application.js
+++ application.js
@@ -240,7 +240,7 @@ function renderHomeHours(container, template, collection){
         var d = moment();
         val.month = moment().month();
         var weekday = moment().format("dddd");
-        val.weekday = moment(weekday).locale('fr-ca');
+        val.weekday = weekday.locale('fr-ca');
         console.log(val.weekday)
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
