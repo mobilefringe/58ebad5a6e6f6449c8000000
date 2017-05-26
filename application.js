f8b4354c1d42be42ea3b882cdc90939b0ecb802f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-26 12:12:54 -0400

    Fleur de Lys 1.0  /application.js

--- application.js
+++ application.js
@@ -182,26 +182,26 @@ function init() {
         });
     });
     
-    function submitToMailChimp(){
-        $("#mce-EMAIL").val($('#fieldEmail').val())
-        $.ajax({
-            type: $("#mc-embedded-subscribe-form").attr('method'),
-            url: $("#mc-embedded-subscribe-form").attr('action'),
-            data: $("#mc-embedded-subscribe-form").serialize(),
-            cache       : false,
-            dataType    : 'json',
-            contentType: "application/json; charset=utf-8",
-            error       : function(err) { alert("Could not connect to the registration server. Please try again later.") },
-            success     : function(data) {
+    // function submitToMailChimp(){
+    //     $("#mce-EMAIL").val($('#fieldEmail').val())
+    //     $.ajax({
+    //         type: $("#mc-embedded-subscribe-form").attr('method'),
+    //         url: $("#mc-embedded-subscribe-form").attr('action'),
+    //         data: $("#mc-embedded-subscribe-form").serialize(),
+    //         cache       : false,
+    //         dataType    : 'json',
+    //         contentType: "application/json; charset=utf-8",
+    //         error       : function(err) { alert("Could not connect to the registration server. Please try again later.") },
+    //         success     : function(data) {
            
-                if (data.result != "success") {
-                    $("#success_subscribe").fadeIn();
-                } else {
-                    $("#success_subscribe").fadeIn();
-                }
-            }
-        })
-    }
+    //             if (data.result != "success") {
+    //                 $("#success_subscribe").fadeIn();
+    //             } else {
+    //                 $("#success_subscribe").fadeIn();
+    //             }
+    //         }
+    //     })
+    // }
 
 }
 
