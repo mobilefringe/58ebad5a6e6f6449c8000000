dcc6069a1039f5e98db8435129502bc70464ac81
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-10 15:24:41 -0400

    Fleur de Lys 1.2  /blog.js

--- blog.js
+++ blog.js
@@ -87,8 +87,6 @@ function renderPosts2(container, template, collection){
         } else {
             val.description_short = val.body;
         }
-        val.post_title = val.title;
-        val.post_slug = val.slug;
         val.counter = counter;
         var date_blog = moment(val.publish_date).tz(getPropertyTimeZone());
         val.published_on = date_blog.format('MMM DD, YYYY');
