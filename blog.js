cef5cbbc74c90a2364bb8aeb44b16be1393c9c07
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-11 10:23:11 -0400

    Fleur de Lys 1.2  /blog.js

--- blog.js
+++ blog.js
@@ -197,5 +197,5 @@ function render_instagram(data){
 
 function getTagCategoris(posts) {
     var tags = _.map(posts, 'tag');
-    console.log("tags", tags);
+    console.log("tags", _.map(posts, 'tag'));
 }
