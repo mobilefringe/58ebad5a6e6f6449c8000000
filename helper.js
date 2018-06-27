9226aaf9d5a541f8d0e59851399d001a222c190e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-27 17:45:11 -0400

    Fleur de Lys 1.2  /helper.js

--- helper.js
+++ helper.js
@@ -113,26 +113,26 @@ function init() {
     }
     
     //Campaign Monitor Sign Up
-    $('#subForm').submit(function (e) {
-        if ($("#agree_terms").prop("checked") != true){
-            alert("Please agree to the term and conditions.");
-            $("#agree_terms").focus();
-            return false;
-        }
-        e.preventDefault();
-        $.getJSON(
-            this.action + "?callback=?",
-            $(this).serialize(),
-            function (data) {
-                if (data.Status === 400) {
-                    alert("Please try again later.");
-                } else { // 200
-                    $('#subForm').trigger('reset');
-                    $("#success_subscribe").fadeIn();
+    // $('#subForm').submit(function (e) {
+    //     if ($("#agree_terms").prop("checked") != true){
+    //         alert("Please agree to the term and conditions.");
+    //         $("#agree_terms").focus();
+    //         return false;
+    //     }
+    //     e.preventDefault();
+    //     $.getJSON(
+    //         this.action + "?callback=?",
+    //         $(this).serialize(),
+    //         function (data) {
+    //             if (data.Status === 400) {
+    //                 alert("Please try again later.");
+    //             } else { // 200
+    //                 $('#subForm').trigger('reset');
+    //                 $("#success_subscribe").fadeIn();
                     
-                }
-        });
-    });
+    //             }
+    //     });
+    // });
     
     //dynamically changing copyright year
     var current_year = moment().year();
